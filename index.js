a41ed0ec7432f0f9aaffa1362a53aab2914009bc
tahmidurrahmann <138325180+tahmidurrahmann@users.noreply.github.com>
2024-01-12 15:09:07 +0600

    added the booking info

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT | 5000;
 require('dotenv').config()
 
@@ -19,12 +20,34 @@ const client = new MongoClient(uri, {
     }
 });
 
+const verifyToken = async (req, res, next) => {
+    if (!req?.headers?.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+    }
+    const token = req?.headers?.authorization.split(" ")[1];
+    jwt.verify(token, process?.env?.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: "unAuthorized Access" })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
+
 async function run() {
     try {
 
         const offerCollection = client.db("travelAgency").collection("offers");
         const userCollection = client.db("travelAgency").collection("users");
         const bookingCollection = client.db("travelAgency").collection("bookings");
+
+        app.post("/jwt", async (req, res) => {
+            const email = req?.body?.email;
+            const token = jwt.sign({ email }, process?.env?.ACCESS_TOKEN, { expiresIn: "24h" });
+            res.send({ token });
+        })
 
         app.get("/offers", async (req, res) => {
             const result = await offerCollection.find().toArray();
@@ -55,30 +78,44 @@ async function run() {
             res.send(result);
         })
 
-        app.get("/bookings/:email", async (req, res) => {
-            const email = req?.params?.email;
+        app.get("/booking", async (req, res) => {
+            const email = req.query.query;
             const query = { userEmail: email };
-            const result = await bookingCollection.findOne(query);
+            const result = await bookingCollection.find(query).toArray();
             res.send(result);
         })
 
         app.post("/bookingInfo", async (req, res) => {
             const bookingInfo = req?.body;
             const tripName = bookingInfo?.tripName;
+            const tickets = bookingInfo?.tickets;
             const result = await bookingCollection.insertOne(bookingInfo);
-            if (result?.insertedId) {
-                const query = { tripName: tripName };
-                const tripInfo = await offerCollection.findOne(query);
-                const totalAvailability = tripInfo?.availability;
-                const filter = { tripName: tripName };
-                const updateDoc = {
-                    $set: {
-                      availability : totalAvailability - 1,
-                    },
-                  };
-                const remainingAvailability = await offerCollection.updateOne(filter,updateDoc)
-                res.send(result);
+            const query = { tripName: tripName };
+            const tripInfo = await offerCollection.findOne(query);
+            const availability = tripInfo?.availability;
+            if (availability >= tickets) {
+                if (result?.insertedId) {
+                    const totalAvailability = tripInfo?.availability;
+                    const filter = { tripName: tripName };
+                    const updateDoc = {
+                        $set: {
+                            availability: totalAvailability - tickets,
+                        },
+                    };
+                    const remainingAvailability = await offerCollection.updateOne(filter, updateDoc)
+                }
+               return res.send(result);
             }
+            else {
+                return res.send("Insufficient ticket availability. Please choose a lower quantity.")
+            }
+        })
+
+        app.delete('/travel-info-delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
         })
 
         await client.db("admin").command({ ping: 1 });
@@ -93,5 +130,5 @@ app.get("/", (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Travel Agency is Traveling on port ${port}`);
-})
+    console.log(`Server is running on port ${port}`);
+});
